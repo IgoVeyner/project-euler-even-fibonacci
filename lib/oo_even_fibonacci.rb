2baa366d14b2e8ca99e7acf7b2a509c2a4d9e9bf
Igor Veyner <igorveyner95@gmail.com>
2021-05-17 13:06:15 -0400

    Done.

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,25 @@
 # Implement your object-oriented solution here!
+
+class EvenFibonacci
+
+  def initialize(num)
+    @num = num
+  end
+  
+  def get_fibonacci
+    sol = [1,2]
+
+    while sol.last < @num
+      sol << (sol.last(2).sum)
+    end
+
+    sol.pop
+    sol
+  end
+
+  def sum
+    sequence = get_fibonacci()
+    sequence.select{ |n| n.even? }.sum
+  end
+
+end
