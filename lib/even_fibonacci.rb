1cae64bbfa1f8c164c42d26ba5cc2ebac346817d
Igor Veyner <igorveyner95@gmail.com>
2021-05-17 13:02:41 -0400

    finish first solution

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,18 @@
 # Implement your procedural solution here!
+def get_fibonacci(limit) 
+  sol = [1,2]
+
+  while sol.last < limit
+    sol << (sol.last(2).sum)
+  end
+
+  sol.pop
+  sol
+end
+
+
+def even_fibonacci_sum(limit)
+  sequence = get_fibonacci(limit)
+
+  sequence.select { |n| n.even? }.sum
+end
